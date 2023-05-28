74d0af0b493a8e0cf5f12cb194b27c971bad5be4
Md. Imran Hossain <emonimran118@gmail.com>
2023-05-28 22:22:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Categories from "./components/categories/categories.component";
+
+function App() {
+  return (
+    <>
+      <Categories />
+    </>
+  );
+}
+
+export default App;
